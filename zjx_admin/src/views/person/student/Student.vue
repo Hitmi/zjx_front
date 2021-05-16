5b7248cedae84666c9dc17605c3ad83a23162167
zhaojunjie-code <1017507941@qq.com>
2021-05-16 22:30:00 +0800

    第一次完结

--- zjx_admin/src/views/person/student/Student.vue
+++ zjx_admin/src/views/person/student/Student.vue
@@ -2,17 +2,30 @@
   <div class="app-container">
     <!--工具按钮-->
     <div style="margin-bottom: 10px">
-
-      <el-button type="danger" size="mini" @click="batchRemove">批量删除</el-button>
+      <el-button type="danger" size="mini" @click="batchRemove"
+        >批量删除</el-button
+      >
       <router-link :to="'/student/create'">
-        <el-button type="primary" size="mini" icon="el-icon-circle-plus-outline">添加</el-button>
+        <el-button type="primary" size="mini" icon="el-icon-circle-plus-outline"
+          >添加</el-button
+        >
       </router-link>
-      <el-button type="primary" size="mini" icon="el-icon-search" @click="searchVisible = true">数据过滤</el-button>
+      <el-button
+        type="primary"
+        size="mini"
+        icon="el-icon-search"
+        @click="searchVisible = true"
+        >数据过滤</el-button
+      >
       <a :href="BASE_API + '/admin/person/student/export'">
-        <el-button size="mini" type="primary" icon="el-icon-download">下载数据</el-button>
+        <el-button size="mini" type="primary" icon="el-icon-download"
+          >下载数据</el-button
+        >
       </a>
       <a href="/static/excelTemplate/StudentList模板.xlsx">
-        <el-button size="mini" type="primary" icon="el-icon-document-copy">下载模板</el-button>
+        <el-button size="mini" type="primary" icon="el-icon-document-copy"
+          >下载模板</el-button
+        >
       </a>
       <el-upload
         :limit="1"
@@ -21,42 +34,57 @@
         :on-success="uploadSuccess"
         :action="BASE_API + '/admin/person/student/upload'"
         class="upload-demo"
-        style="display:inline;">
-        <el-button size="mini" type="primary" icon="el-icon-upload2">导入数据</el-button>
+        style="display: inline"
+      >
+        <el-button size="mini" type="primary" icon="el-icon-upload2"
+          >导入数据</el-button
+        >
       </el-upload>
     </div>
 
     <!--查询表单-->
-    <el-drawer
-      :visible.sync="searchVisible"
-      direction="rtl"
-      size="30%">
+    <el-drawer :visible.sync="searchVisible" direction="rtl" size="30%">
       <template v-slot="">
-        <h2 style="color: #409EFF"><i class="el-icon-search" style="margin-bottom: 30px"/>数据过滤</h2>
+        <h2 style="color: #409eff">
+          <i class="el-icon-search" style="margin-bottom: 30px" />数据过滤
+        </h2>
       </template>
-      <student-search @click-get-data="filterData"/>
+      <student-search @click-get-data="filterData" />
     </el-drawer>
 
     <!-- 表格 -->
-    <el-table :data="list" border stripe @selection-change="handleSelectionChange">
-      <el-table-column type="selection"/>
-      <el-table-column
-        label="#"
-        width="50">
+    <el-table
+      :data="list"
+      border
+      stripe
+      @selection-change="handleSelectionChange"
+    >
+      <el-table-column type="selection" />
+      <el-table-column label="#" width="50">
         <template slot-scope="scope">
           {{ (page - 1) * limit + scope.$index + 1 }}
         </template>
       </el-table-column>
-      <el-table-column prop="username" label="学号" width="200" align="center"/>
+      <el-table-column
+        prop="username"
+        label="学号"
+        width="200"
+        align="center"
+      />
 
-      <el-table-column prop="name" label="姓名" width="100" align="center"/>
+      <el-table-column prop="name" label="姓名" width="100" align="center" />
       <el-table-column prop="sex" label="性别" width="100" align="center">
         <template slot-scope="scope">
           <el-tag v-if="scope.row.sex === 0">女</el-tag>
           <el-tag v-if="scope.row.sex === 1">男</el-tag>
         </template>
       </el-table-column>
-      <el-table-column prop="graduateDate" label="毕业日期" width="200" align="center"/>
+      <el-table-column
+        prop="graduateDate"
+        label="毕业日期"
+        width="200"
+        align="center"
+      />
       <el-table-column prop="sex" label="账号状态" width="150" align="center">
         <template slot-scope="scope">
           <el-tag v-if="scope.row.disabled === 0">启用</el-tag>
@@ -65,15 +93,24 @@
       </el-table-column>
       <el-table-column label="操作" width="350" fixed="right" align="center">
         <template slot-scope="scope">
-          <el-button size="mini" type="info" @click="showDialog(scope.row.id)">查看</el-button>
+          <el-button size="mini" type="info" @click="showDialog(scope.row.id)"
+            >查看</el-button
+          >
+          <el-button size="mini" type="danger" @click="removeById(scope.row.id)"
+            >删除
+          </el-button>
           <el-button
             size="mini"
             type="danger"
-            @click="removeById(scope.row.id)">删除
-          </el-button>
-          <el-button size="mini" type="danger" @click="changeState(scope.row.id)">启/禁用</el-button>
-          <el-button size="mini" type="danger" @click="resetPassword(scope.row.id)">重置密码</el-button>
-
+            @click="changeState(scope.row.id)"
+            >启/禁用</el-button
+          >
+          <el-button
+            size="mini"
+            type="danger"
+            @click="resetPassword(scope.row.id)"
+            >重置密码</el-button
+          >
         </template>
       </el-table-column>
     </el-table>
@@ -87,23 +124,29 @@
       style="padding: 30px 0; text-align: center"
       layout="sizes, prev, pager, next, jumper, ->, total"
       @current-change="changeCurrentPage"
-      @size-change="changePageSize"/>
+      @size-change="changePageSize"
+    />
 
     <!--弹出窗口，用于显示详细信息-->
-    <el-dialog :visible.sync="dialogTableVisible" title="详细信息" width="650px" center>
-      <student-detail :student="student"/>
-    </el-dialog>
-
+    <div v-if="dialogTableVisible">
+      <el-dialog
+        :visible.sync="dialogTableVisible"
+        title="详细信息"
+        width="650px"
+        center
+      >
+        <student-detail :student="student" />
+      </el-dialog>
+    </div>
   </div>
 </template>
 
 <script>
-import studentApi from '@/api/person/student'
-import StudentSearch from '@/views/person/student/StudentSearch'
-import StudentDetail from '@/views/person/student/StudentDetail'
+import studentApi from "@/api/person/student";
+import StudentSearch from "@/views/person/student/StudentSearch";
+import StudentDetail from "@/views/person/student/StudentDetail";
 
 export default {
-
   components: { StudentDetail, StudentSearch },
 
   data() {
@@ -113,16 +156,16 @@ export default {
       page: 1, // 页码
       limit: 10, // 每页记录数
       searchObj: {
-        name: '',
-        sex: '',
-        birth: '',
-        graduateDate: '',
-        clazz: '',
-        departmentId: '',
-        mobile: '',
-        wxId: '',
-        avatar: '',
-        sign: ''
+        name: "",
+        sex: "",
+        birth: "",
+        graduateDate: "",
+        clazz: "",
+        departmentId: "",
+        mobile: "",
+        wxId: "",
+        avatar: "",
+        sign: "",
       }, // 查询表单
       multipleSelection: [], // 批量删除选中的记录列表
       fileList: [],
@@ -131,157 +174,165 @@ export default {
       BASE_API: process.env.BASE_API,
       // 讲师对象
       student: {
-        id: '',
-        name: '',
-        sex: '',
-        birth: '',
-        graduateDate: '',
-        clazz: '',
-        departmentId: '',
-        mobile: '',
-        wxId: '',
-        avatar: '',
-        sign: ''
-      }
-    }
+        id: "",
+        name: "",
+        sex: "",
+        birth: "",
+        graduateDate: "",
+        clazz: "",
+        departmentId: "",
+        mobile: "",
+        wxId: "",
+        avatar: "",
+        sign: "",
+      },
+    };
   },
   created() {
-    this.getData()
+    this.getData();
   },
 
   methods: {
     // 更改学生状态
     changeState(id) {
-      studentApi.changeState(id).then(response => {
+      studentApi.changeState(id).then((response) => {
         this.$message({
           message: response.message,
-          type: 'success'
-        })
-      })
-      this.getData()
+          type: "success",
+        });
+      });
+      this.getData();
     },
     // 重置学生密码
     resetPassword(id) {
-      studentApi.resetPassword(id).then(response => {
+      studentApi.resetPassword(id).then((response) => {
         this.$message({
           message: response.message,
-          type: 'success'
-        })
-      })
+          type: "success",
+        });
+      });
     },
     // 显示详细信息弹窗
     showDialog(id) {
-      this.dialogTableVisible = true
-      studentApi.getById(id).then(response => {
-        this.student = response.data.item
-      })
+      this.dialogTableVisible = true;
+      studentApi.getById(id).then((response) => {
+        this.student = response.data.item;
+      });
     },
     // 导入excel失败后调用
     uploadError() {
-      this.fileList = []
+      this.fileList = [];
     },
     // 导入excel成功后调用
     uploadSuccess() {
-      this.fileList = []
-      this.getData()
+      this.fileList = [];
+      this.getData();
     },
     // 调用api模块，加载  列表数据
     getData() {
-      console.log('getData>>>', this.searchObj)
+      console.log("getData>>>", this.searchObj);
 
-      studentApi.pageList(this.page, this.limit, this.searchObj).then(response => {
-        this.list = response.data.list
-        this.total = response.data.total
-      })
+      studentApi
+        .pageList(this.page, this.limit, this.searchObj)
+        .then((response) => {
+          this.list = response.data.list;
+          this.total = response.data.total;
+        });
     },
     filterData(data) {
-      console.log('filterData>>>', data)
-      studentApi.pageList(this.page, this.limit, data).then(response => {
-        this.list = response.data.list
-        this.total = response.data.total
-      })
+      console.log("filterData>>>", data);
+      studentApi.pageList(this.page, this.limit, data).then((response) => {
+        this.list = response.data.list;
+        this.total = response.data.total;
+      });
     },
     // 改编页码
     changeCurrentPage(page) {
-      this.page = page
-      this.getData()
+      this.page = page;
+      this.getData();
     },
 
     // 改变每页记录数
     changePageSize(size) {
-      console.log('changePageSize:' + size)
-      this.limit = size
-      this.getData()
+      console.log("changePageSize:" + size);
+      this.limit = size;
+      this.getData();
     },
 
     // 重置表单
     resetData() {
-      this.searchObj = {}
-      this.getData()
+      this.searchObj = {};
+      this.getData();
     },
 
     // 删除记录
     removeById(id) {
       // 询问是否删除
-      this.$confirm('此操作将永久删除该数据, 是否继续?', '提示', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        type: 'warning'
-      }).then(() => {
-        return studentApi.removeById(id)
-      }).then(response => {
-        // 刷新页面
-        this.getData()
-        // 弹出成功提示
-        this.$message({
-          message: response.message,
-          type: 'success'
-        })
-      }).catch((err) => {
-        console.log(err)
-        if (err === 'cancel') {
-          this.$message({
-            type: 'info',
-            message: '已取消删除'
-          })
-        }
+      this.$confirm("此操作将永久删除该数据, 是否继续?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
       })
+        .then(() => {
+          return studentApi.removeById(id);
+        })
+        .then((response) => {
+          // 刷新页面
+          this.getData();
+          // 弹出成功提示
+          this.$message({
+            message: response.message,
+            type: "success",
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+          if (err === "cancel") {
+            this.$message({
+              type: "info",
+              message: "已取消删除",
+            });
+          }
+        });
     },
     batchRemove() {
-      console.log('removeRows......')
+      console.log("removeRows......");
 
       if (this.multipleSelection.length === 0) {
-        this.$message.warning('请选择要删除的记录！')
-        return
+        this.$message.warning("请选择要删除的记录！");
+        return;
       }
 
-      this.$confirm('此操作将永久批量删除数据, 是否继续?', '提示', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        type: 'warning'
-      }).then(() => {
-        // 点击确定，远程调用ajax
-        // 遍历selection，将id取出放入id列表
-        var idList = []
-        this.multipleSelection.forEach(item => {
-          idList.push(item.id)
-        })
-        // 调用api
-        return studentApi.batchRemove(idList)
-      }).then((response) => {
-        this.getData()
-        this.$message.success(response.message)
-      }).catch(error => {
-        if (error === 'cancel') {
-          this.$message.info('取消删除')
-        }
+      this.$confirm("此操作将永久批量删除数据, 是否继续?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
       })
+        .then(() => {
+          // 点击确定，远程调用ajax
+          // 遍历selection，将id取出放入id列表
+          var idList = [];
+          this.multipleSelection.forEach((item) => {
+            idList.push(item.id);
+          });
+          // 调用api
+          return studentApi.batchRemove(idList);
+        })
+        .then((response) => {
+          this.getData();
+          this.$message.success(response.message);
+        })
+        .catch((error) => {
+          if (error === "cancel") {
+            this.$message.info("取消删除");
+          }
+        });
     },
     // 当多选选项发生变化的时候调用
     handleSelectionChange(selection) {
-      console.log(selection)
-      this.multipleSelection = selection
-    }
-  }
-}
+      console.log(selection);
+      this.multipleSelection = selection;
+    },
+  },
+};
 </script>
--- zjx_admin/src/views/person/teacher/Teacher.vue
+++ zjx_admin/src/views/person/teacher/Teacher.vue
@@ -2,17 +2,30 @@
   <div class="app-container">
     <!-- 工具按钮 -->
     <div style="margin-bottom: 10px">
-
-      <el-button type="danger" size="mini" @click="batchRemove">批量删除</el-button>
+      <el-button type="danger" size="mini" @click="batchRemove"
+        >批量删除</el-button
+      >
       <router-link :to="'/teacher/create'">
-        <el-button type="primary" size="mini" icon="el-icon-circle-plus-outline">添加</el-button>
+        <el-button type="primary" size="mini" icon="el-icon-circle-plus-outline"
+          >添加</el-button
+        >
       </router-link>
-      <el-button type="primary" size="mini" icon="el-icon-search" @click="showSearch = true">数据过滤</el-button>
+      <el-button
+        type="primary"
+        size="mini"
+        icon="el-icon-search"
+        @click="showSearch = true"
+        >数据过滤</el-button
+      >
       <a :href="BASE_API + '/admin/person/teacher/export'">
-        <el-button size="mini" type="primary" icon="el-icon-download">下载数据</el-button>
+        <el-button size="mini" type="primary" icon="el-icon-download"
+          >下载数据</el-button
+        >
       </a>
       <a href="/static/excelTemplate/TeacherList模板.xlsx">
-        <el-button size="mini" type="primary" icon="el-icon-document-copy">下载模板</el-button>
+        <el-button size="mini" type="primary" icon="el-icon-document-copy"
+          >下载模板</el-button
+        >
       </a>
       <el-upload
         :limit="1"
@@ -21,33 +34,38 @@
         :on-success="uploadSuccess"
         :action="BASE_API + '/admin/person/teacher/upload'"
         class="upload-demo"
-        style="display:inline;">
-        <el-button size="mini" type="primary" icon="el-icon-upload2">导入数据</el-button>
+        style="display: inline"
+      >
+        <el-button size="mini" type="primary" icon="el-icon-upload2"
+          >导入数据</el-button
+        >
       </el-upload>
     </div>
 
     <!--查询表单-->
-    <el-drawer
-      :visible.sync="showSearch"
-      direction="rtl"
-      size="30%">
-      <template >
-        <h2 style="color: #409EFF"><i class="el-icon-search" style="margin-bottom: 30px"/>数据过滤</h2>
+    <el-drawer :visible.sync="showSearch" direction="rtl" size="30%">
+      <template>
+        <h2 style="color: #409eff">
+          <i class="el-icon-search" style="margin-bottom: 30px" />数据过滤
+        </h2>
       </template>
-      <teacher-search @click-get-data="getData"/>
+      <teacher-search @click-get-data="getData" />
     </el-drawer>
 
     <!-- 表格 -->
-    <el-table :data="list" border stripe @selection-change="handleSelectionChange">
-      <el-table-column type="selection"/>
-      <el-table-column
-        label="#"
-        width="50">
+    <el-table
+      :data="list"
+      border
+      stripe
+      @selection-change="handleSelectionChange"
+    >
+      <el-table-column type="selection" />
+      <el-table-column label="#" width="50">
         <template slot-scope="scope">
           {{ (page - 1) * limit + scope.$index + 1 }}
         </template>
       </el-table-column>
-      <el-table-column prop="name" label="姓名" width="100" align="center"/>
+      <el-table-column prop="name" label="姓名" width="100" align="center" />
       <el-table-column prop="level" label="头衔" width="100" align="center">
         <template slot-scope="scope">
           <el-tag v-if="scope.row.level === 1">教授</el-tag>
@@ -63,15 +81,20 @@
           <el-tag v-if="scope.row.sex === 2">女</el-tag>
         </template>
       </el-table-column>
-      <el-table-column prop="intro" label="介绍" align="center"/>
+      <el-table-column prop="intro" label="介绍" align="center" />
       <el-table-column label="操作" width="250" fixed="right" align="center">
         <template slot-scope="scope">
-          <el-button size="mini" type="info" @click="showDialog(scope.row.id)">查看</el-button>
-          <el-button size="mini" type="danger" @click="resetPassword(scope.row.id)">重置密码</el-button>
+          <el-button size="mini" type="info" @click="showDialog(scope.row.id)"
+            >查看</el-button
+          >
           <el-button
             size="mini"
             type="danger"
-            @click="removeById(scope.row.id)">删除
+            @click="resetPassword(scope.row.id)"
+            >重置密码</el-button
+          >
+          <el-button size="mini" type="danger" @click="removeById(scope.row.id)"
+            >删除
           </el-button>
         </template>
       </el-table-column>
@@ -86,25 +109,30 @@
       style="padding: 30px 0; text-align: center"
       layout="sizes, prev, pager, next, jumper, ->, total"
       @current-change="changeCurrentPage"
-      @size-change="changePageSize"/>
+      @size-change="changePageSize"
+    />
 
     <!--弹出窗口，用于显示详细信息-->
     <div v-if="dialogTableVisible">
-    <el-dialog :visible.sync="dialogTableVisible" title="详细信息" width="700px" center>
-      <teacher-detail :teacher="teacher"></teacher-detail>
-    </el-dialog>
+      <el-dialog
+        :visible.sync="dialogTableVisible"
+        title="详细信息"
+        width="700px"
+        center
+      >
+        <teacher-detail :teacher="teacher"></teacher-detail>
+      </el-dialog>
     </div>
   </div>
 </template>
 
 <script>
-import teacherApi from '@/api/person/teacher'
-import departmentApi from '@/api/person/department'
-import TeacherSearch from '@/views/person/teacher/TeacherSearch'
-import TeacherDetail from '@/views/person/teacher/TeacherDetail'
+import teacherApi from "@/api/person/teacher";
+import departmentApi from "@/api/person/department";
+import TeacherSearch from "@/views/person/teacher/TeacherSearch";
+import TeacherDetail from "@/views/person/teacher/TeacherDetail";
 
 export default {
-
   components: { TeacherDetail, TeacherSearch },
 
   data() {
@@ -114,9 +142,9 @@ export default {
       page: 1, // 页码
       limit: 10, // 每页记录数
       searchObj: {
-        name: '',
-        level: '',
-        departmentId: ''
+        name: "",
+        level: "",
+        departmentId: "",
       }, // 查询表单
       multipleSelection: [], // 批量删除选中的记录列表
       departmentList: [], // 部门列表
@@ -125,147 +153,153 @@ export default {
       BASE_API: process.env.BASE_API,
       // 讲师对象
       teacher: {
-        id: '12',
-        name: '21',
+        id: "12",
+        name: "21",
         sex: 1,
-        birth: '12',
-        departmentId: '21',
-        avatar: '21',
-        intro: '21',
-        level: '21'
+        birth: "12",
+        departmentId: "21",
+        avatar: "21",
+        intro: "21",
+        level: "21",
       },
-      showSearch: false
-    }
+      showSearch: false,
+    };
   },
 
   created() {
-    this.getDepartment()
-    this.getData()
+    this.getDepartment();
+    this.getData();
   },
 
   methods: {
     // 显示详细信息弹窗
     showDialog(id) {
-      teacherApi.getById(id).then(response => {
+      teacherApi.getById(id).then((response) => {
         //console.log(response.data.item);
         this.teacher = response.data.item;
-         this.dialogTableVisible = true
-      })
+        this.dialogTableVisible = true;
+      });
     },
     // 导入excel失败后调用
     uploadError(err) {
-      console.log(err)
+      console.log(err);
     },
     // 导入excel成功后调用
     uploadSuccess() {
-      this.fileList = []
-      this.getData()
+      this.fileList = [];
+      this.getData();
     },
     // 调用api模块，加载讲师列表
     getData(data) {
-      teacherApi.pageList(this.page, this.limit, data).then(response => {
+      teacherApi.pageList(this.page, this.limit, data).then((response) => {
         // console.log(response);
-        this.list = response.data.list
-        this.total = response.data.total
-      })
+        this.list = response.data.list;
+        this.total = response.data.total;
+      });
     },
     // 获取部门信息
     getDepartment() {
-      departmentApi.list().then(response => {
-        this.departmentList = response.data.list
-      })
+      departmentApi.list().then((response) => {
+        this.departmentList = response.data.list;
+      });
     },
     // 改编页码
     changeCurrentPage(page) {
-      console.log('changeCurrentPage:' + page)
-      this.page = page
-      this.getData()
+      console.log("changeCurrentPage:" + page);
+      this.page = page;
+      this.getData();
     },
 
     // 改变每页记录数
     changePageSize(size) {
-      console.log('changePageSize:' + size)
-      this.limit = size
-      this.getData()
+      console.log("changePageSize:" + size);
+      this.limit = size;
+      this.getData();
     },
 
     // 重置表单
     resetData() {
-      this.searchObj = {}
-      this.getData()
+      this.searchObj = {};
+      this.getData();
     },
 
     // 删除记录
     removeById(id) {
       // 询问是否删除
-      this.$confirm('此操作将永久删除该数据, 是否继续?', '提示', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        type: 'warning'
-      }).then(() => {
-        return teacherApi.removeById(id)
-      }).then(response => {
-        // 刷新页面
-        this.getData()
-        // 弹出成功提示
-        this.$message({
-          message: response.message,
-          type: 'success'
-        })
-      }).catch((err) => {
-        console.log(err)
-        if (err === 'cancel') {
-          this.$message({
-            type: 'info',
-            message: '已取消删除'
-          })
-        }
+      this.$confirm("此操作将永久删除该数据, 是否继续?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
       })
+        .then(() => {
+          return teacherApi.removeById(id);
+        })
+        .then((response) => {
+          // 刷新页面
+          this.getData();
+          // 弹出成功提示
+          this.$message({
+            message: response.message,
+            type: "success",
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+          if (err === "cancel") {
+            this.$message({
+              type: "info",
+              message: "已取消删除",
+            });
+          }
+        });
     },
     batchRemove() {
-      console.log('removeRows......')
+      console.log("removeRows......");
 
       if (this.multipleSelection.length === 0) {
-        this.$message.warning('请选择要删除的记录！')
-        return
+        this.$message.warning("请选择要删除的记录！");
+        return;
       }
 
-      this.$confirm('此操作将永久批量删除数据, 是否继续?', '提示', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        type: 'warning'
-      }).then(() => {
-        // 点击确定，远程调用ajax
-        // 遍历selection，将id取出放入id列表
-        var idList = []
-        this.multipleSelection.forEach(item => {
-          idList.push(item.id)
-        })
-        // 调用api
-        return teacherApi.batchRemove(idList)
-      }).then((response) => {
-        this.getData()
-        this.$message.success(response.message)
-      }).catch(error => {
-        if (error === 'cancel') {
-          this.$message.info('取消删除')
-        }
+      this.$confirm("此操作将永久批量删除数据, 是否继续?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
       })
+        .then(() => {
+          // 点击确定，远程调用ajax
+          // 遍历selection，将id取出放入id列表
+          var idList = [];
+          this.multipleSelection.forEach((item) => {
+            idList.push(item.id);
+          });
+          // 调用api
+          return teacherApi.batchRemove(idList);
+        })
+        .then((response) => {
+          this.getData();
+          this.$message.success(response.message);
+        })
+        .catch((error) => {
+          if (error === "cancel") {
+            this.$message.info("取消删除");
+          }
+        });
     },
     // 当多选选项发生变化的时候调用
     handleSelectionChange(selection) {
-      console.log(selection)
-      this.multipleSelection = selection
+      console.log(selection);
+      this.multipleSelection = selection;
     },
     // 重置学生密码
     resetPassword(id) {
-      teacherApi.resetPassword(id).then(response => {
+      teacherApi.resetPassword(id).then((response) => {
         this.$message({
           message: response.message,
-          type: 'success'
-        })
-      })
-    }
-  }
-}
+          type: "success",
+        });
+      });
+    },
+  },
+};
 </script>
